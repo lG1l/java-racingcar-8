02c79442404179bfae8620045ebbeed483798bbb
lG1l <suhjw123@naver.com>
2025-10-27 21:41:06 +0900

    feat(실행흐름 제어): Controller클래스에서 사용자가 입력한 횟수만큼 움직이고 각 라운드 출력하게끔 구현

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -8,11 +8,20 @@ public class Controller {
 
         int count = IOConsole.inputCount();
         checkCount(count);
+
+        countMove(cars, count);
     }
 
     private void checkCount(int count) {
         if (count < 0) {
             throw new IllegalArgumentException("수행횟수가 음수입니다.");
+        }
+    }
+
+    private void countMove(Cars cars, int count) {
+        for (int i = 0; i < count; i++) {
+            String comment = cars.raceOnce();
+            IOConsole.roundResultPrint(comment);
         }
     }
 }
