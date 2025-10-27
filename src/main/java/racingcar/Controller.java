2600c272b943c7bd74bdb548c3405766851072ed
lG1l <suhjw123@naver.com>
2025-10-27 21:15:26 +0900

    feat(입출력): 입출력을 담당하는 IOConsole 클래스 추가

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -6,4 +6,10 @@ public class Controller {
     public void run() {
 
     }
+
+    private void checkCount(int count) {
+        if (count < 0) {
+            throw new IllegalArgumentException("수행횟수가 음수입니다.");
+        }
+    }
 }
